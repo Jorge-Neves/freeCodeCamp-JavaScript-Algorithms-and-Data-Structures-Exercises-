8b3ee74cbee1720d0e8f73a54cfd00ae62cdb4c6
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-10-31 03:38:41 +0000

    basic JavaScript 1 + 2

---
+++ Basic-JavaScript/01-Comment-Your-JavaScript-Code.js
@@ -0,0 +1,18 @@
+// Prompt
+
+// Try creating one of each type of comment.
+// Tests
+// You should create a // style comment that contains at least five letters.
+
+// You should create a /* */ style comment that contains at least five letters.
+
+
+
+
+// Solution
+
+// in-line comment
+
+/* Multi-line comment */
+
+
---
+++ Basic-JavaScript/02-Declare-JavaScript-Variables.js
@@ -0,0 +1,11 @@
+// prompt
+// Use the var keyword to create a variable called myName.
+
+// Tests
+// You should declare myName with the var keyword, ending with a semicolon
+
+
+// Solution
+
+
+var myName = Paulo;
