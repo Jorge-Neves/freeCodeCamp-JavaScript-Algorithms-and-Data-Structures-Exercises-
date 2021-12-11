2d86a343668b6920d5cea94d3745238d1a6a5bd9
Paulo Santos <paulo.jorge.ngs@gmail.com>
2021-12-11 23:59:39 +0000

    Create 05-Initializing-Variables-with-the-Assignment-Operator.js

---
+++ Basic-JavaScript/05-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,19 @@
+// prompt
+// Assign the value 7 to the variable a
+
+// Setup
+// var a;
+// a = 7;
+// var b;
+
+// Tests
+// You should not change code above the specified comment.
+// a should have a value of 7.
+
+
+// Solution
+
+var a;
+a = 7;
+var b;
+b = a;
