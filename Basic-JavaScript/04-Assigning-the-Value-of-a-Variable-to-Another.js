746e95b5f730cb8061bc6cc6f0a73080d246df04
Paulo Santos <paulo.jorge.ngs@gmail.com>
2021-12-11 23:56:18 +0000

    Create 04-Assigning-the-Value-of-a-Variable-to-Another.js

---
+++ Basic-JavaScript/04-Assigning-the-Value-of-a-Variable-to-Another.js
@@ -0,0 +1,15 @@
+// prompt
+// Assign the value 7 to the variable a
+
+// code provided
+// var a;
+
+// Tests
+// You should not change code above the specified comment.
+// a should have a value of 7.
+
+
+// Solution
+
+var a;
+a = 7;
